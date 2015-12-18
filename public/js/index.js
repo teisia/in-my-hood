e3b6040cdef13787463e3fe4fccd2b547ddf083a
Teisia Park <teisia@gmail.com>
2015-12-17 17:15:32 -0700

    added preview functionality and icons

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+
+
 var gMap = $.ajax ({
 url: "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCQilPj6BWogLeB6QgDBqI7HuownF92CzA",
 method: "POST"
@@ -10,13 +12,13 @@ var latlng = lat.concat(lng);
 var artist = document.getElementById('artistField');
 
 $("#buttoni").click(function() {
+var previewIcon = "speaker.png";
+var ticketIcon = "ticket.png";
 var ll = $.ajax ({
   url: "https://api.bandsintown.com/artists/"+artist.value+"/events/recommended?location="+latlng+"&radius=20&app_id=in-my-hood&api_version=2.0&format=json",
   method: "GET",
   dataType: "jsonp"
 })
-
-//var datesArray = [];
 
 ll.done(function (response) {
   if (response.length === 0) {
@@ -25,17 +27,33 @@ ll.done(function (response) {
 for (var i = 0; i < response.length; i++) {
   var artistNumber = response[i]["artists"].length;
   var dateCat = (response[i]["datetime"]).substring(0,10);
-  //datesArray.push(dateCat);
   var dateNice = response[i]["formatted_datetime"];
   var dateNicer = dateNice.substring(0, dateNice.length - 16);
   var tickets = response[i]["ticket_url"];
-  console.log(tickets);
-  //if(datesArray[i] === datesArray[i + 1])
+
  for (var j = 0; j < artistNumber; j++) {
-$("#concertList").append("<tr><td id='date'>"+dateNicer+"</td></tr><tr><td><img src='"+response[i]['artists'][j]['thumb_url']+"'/></td><td id='name'>"+response[i]['artists'][j]['name']+"<br>"+response[i]['venue']['name']+" - "+response[i]['venue']['city']+", "+response[i]['venue']['region']+"<br><a href='"+tickets+"' onclick='window.open(this.href); return false;'><button id='more'>get tickets</button></a></td></tr>");
+     var artistname = response[i]['artists'][j]['name'];
+     var preview = $.ajax ({
+       url: "https://itunes.apple.com/search?term="+response[i]['artists'][j]['name']+"",
+       method: "GET",
+       dataType: "jsonp"
+       })
+     console.log("response["+i+"]['artists']["+j+"]['name'] = "+response[i]['artists'][j]['name']);
+       console.log("artistname = "+artistname);
+$("#concertList").append("<tr><td id='date'>"+dateNicer+"</td></tr><tr><td><img src='"+response[i]['artists'][j]['thumb_url']+"'/></td><td id='name'>"+response[i]['artists'][j]['name']+"<br>"+response[i]['venue']['name']+" - "+response[i]['venue']['city']+", "+response[i]['venue']['region']+"<br><div class='prevTrack'><img id='"+artistname+"'class ='speaker' src='"+previewIcon+"'/></div><a href='"+tickets+"' onclick='window.open(this.href); return false;'><div id='more'><img id= 'ticket' src='"+ticketIcon+"'></div></a></td></tr>");
+
+preview.done(function(payload) {
+  $(".speaker").click(function() {
+    $("#concertList").remove("tr");
+    if(this.id === payload["results"][0]["artistName"]) {
+       var previewOne = payload["results"][0];
+         $(this).parents("td").append("<tr><td><audio src='"+previewOne['previewUrl']+"' controls></audio></td></tr>");
+    }
+ })
+})
  }
- }
- }
+}
+}
 })
 })
 })
